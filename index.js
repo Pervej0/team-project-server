ad176570f91714fd2ce58be15db0e94f1b11d937
pervej0 <mdparvez222khan@gmail.com>
2022-02-16 15:13:12 +0600

    chat backend created

--- index.js
+++ index.js
@@ -38,7 +38,31 @@ app.get("/", (req, res) => {
 });
 
 // socket io for chat
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
+io.on("connection", (socket) => {
+  //   console.log("User", socket.id);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    // console.log(`${socket.id} room is ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("received_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    // console.log("Disconnected", socket.id);
+  });
+});
+
+// server listening
 server.listen(port, () => {
   console.log("Server is running on ", port);
 });
